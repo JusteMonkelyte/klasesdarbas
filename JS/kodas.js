d3d291d79f96e786fe2c1cf7eb2f2388f6b8d0e6
Juste Monkelyte <justemonkelyte@gmail.com>
2020-10-14 20:48:12 +0300

    Sukurem navigacija

---
+++ JS/kodas.js
@@ -0,0 +1,42 @@
+//Pirma uzduotis
+
+let a= 1 ;
+console.log('Kintamojo reikšmė:'+a)
+
+a = 2
+console.log('Kintamojo reikšmė:'+a)
+
+//Antra uzduotis
+
+b = 'Justė Monkelytė'
+c = 'Mano vardas'
+
+console.log (c+' '+ b)
+
+//Trecia uzduotis
+
+let metai = 1989;
+let menuo = 9;
+let diena = 26;
+
+console.log (metai % 10 + menuo % 10 + diena % 10);
+
+//Ketvirta uzduotis
+let r = 15
+let pi=3.14
+
+console.log (2 * pi * r);
+
+//Antros dalies 1 užduotis
+
+
+let  vardas = 'Tomas';
+     pavarde = 'Tomauskas';
+     kursas = '4' ;
+     universitetas = 'KTU';
+     pažymiai = [7,4,5];
+
+     console.log (vardas + ' ' + pavarde + ' mokosi ' + universitetas + ',' + kursas + ' kurse,' + 'jo pažymiai '+ pažymiai);
+
+
+
